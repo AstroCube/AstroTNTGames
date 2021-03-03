9f5a86684ceedd3c9b42c7302e14739e1611cc86
Ian Castaño <ianfe@martina.com.co>
2021-03-02 23:56:04 -0500

    Added announce at start

--- Run/src/main/java/net/astrocube/tnt/run/spawn/CorePlayerSpawner.java
+++ Run/src/main/java/net/astrocube/tnt/run/spawn/CorePlayerSpawner.java
@@ -1,6 +1,8 @@
 package net.astrocube.tnt.run.spawn;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import me.yushust.message.MessageHandler;
 import net.astrocube.api.bukkit.game.event.match.MatchInvalidateEvent;
 import net.astrocube.api.bukkit.game.map.configuration.CoordinatePoint;
 import org.bukkit.Bukkit;
@@ -13,8 +15,10 @@ import java.util.Set;
 @Singleton
 public class CorePlayerSpawner implements PlayerSpawner {
 
+    private @Inject MessageHandler messageHandler;
+
     @Override
-    public void spawn(Set<Player> players, String match, CoordinatePoint point) {
+    public void spawn(Player player, String match, CoordinatePoint point) {
 
         World world = Bukkit.getWorld("match_" + match);
 
@@ -25,7 +29,13 @@ public class CorePlayerSpawner implements PlayerSpawner {
 
         Location location = new Location(world, point.getX(), point.getY(), point.getZ());
 
-        players.forEach(player -> player.teleport(location));
+        player.teleport(location);
 
     }
+
+    @Override
+    public void announce(Player player) {
+        messageHandler.send(player, "game.help");
+    }
+
 }
--- Run/src/main/java/net/astrocube/tnt/run/spawn/PlayerSpawner.java
+++ Run/src/main/java/net/astrocube/tnt/run/spawn/PlayerSpawner.java
@@ -10,10 +10,16 @@ public interface PlayerSpawner {
     /**
      * Performs spawn for every player at
      * configured location.
-     * @param players to spawn
+     * @param player to spawn
      * @param match of the requesting world
      * @param point of the spawn
      */
-    void spawn(Set<Player> players, String match, CoordinatePoint point);
+    void spawn(Player player, String match, CoordinatePoint point);
+
+    /**
+     * Displays help tip at beginning for the user
+     * @param player to announce
+     */
+    void announce(Player player);
 
 }
