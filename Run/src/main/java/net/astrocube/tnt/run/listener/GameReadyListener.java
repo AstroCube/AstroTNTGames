ae8d16f58eb452418637f398307fa1adbfeb6901
Ian Castaño <ianfe@martina.com.co>
2021-03-03 00:01:35 -0500

    Added announcement and teleport for every spawned player

--- Run/src/main/java/net/astrocube/tnt/run/listener/GameReadyListener.java
+++ Run/src/main/java/net/astrocube/tnt/run/listener/GameReadyListener.java
@@ -4,8 +4,12 @@ import com.google.inject.Inject;
 import net.astrocube.api.bukkit.game.event.game.GameReadyEvent;
 import net.astrocube.api.bukkit.game.event.match.MatchInvalidateEvent;
 import net.astrocube.api.bukkit.game.exception.GameControlException;
+import net.astrocube.api.bukkit.game.map.MapConfigurationProvider;
+import net.astrocube.api.bukkit.game.match.control.MatchParticipantsProvider;
 import net.astrocube.api.bukkit.virtual.game.match.Match;
 import net.astrocube.api.core.service.find.FindService;
+import net.astrocube.tnt.run.map.MapConfiguration;
+import net.astrocube.tnt.run.spawn.PlayerSpawner;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,18 +20,34 @@ import java.util.logging.Level;
 public class GameReadyListener implements Listener {
 
     private @Inject FindService<Match> findService;
+    private @Inject MapConfigurationProvider mapConfigurationProvider;
     private @Inject Plugin plugin;
+
+    private @Inject PlayerSpawner playerSpawner;
 
     @EventHandler
     public void onGameReady(GameReadyEvent event) {
 
         findService.find(event.getMatch()).callback(response -> {
+
             try {
 
-                if (!response.isSuccessful() || !response.getResponse().isPresent()) {
+                if (!response.isSuccessful()) {
                     throw new GameControlException("The requested match was not found");
                 }
 
+
+                MapConfiguration configuration =
+                        mapConfigurationProvider.parseConfiguration(event.getConfiguration(), MapConfiguration.class);
+
+                response.ifSuccessful(match -> {
+
+                    MatchParticipantsProvider.getOnlinePlayers(match).forEach(player -> {
+                        playerSpawner.spawn(player, match.getId(), configuration.getSpawn());
+                        playerSpawner.announce(player);
+                    });
+
+                });
 
 
             } catch (Exception e) {
--- Run/src/main/java/net/astrocube/tnt/run/loader/InjectionLoaderModule.java
+++ Run/src/main/java/net/astrocube/tnt/run/loader/InjectionLoaderModule.java
@@ -1,6 +1,7 @@
 package net.astrocube.tnt.run.loader;
 
 import me.fixeddev.inject.ProtectedModule;
+import net.astrocube.tnt.run.spawn.SpawnModule;
 import net.astrocube.tnt.translation.TranslationModule;
 
 public class InjectionLoaderModule extends ProtectedModule {
@@ -9,6 +10,7 @@ public class InjectionLoaderModule extends ProtectedModule {
     public void configure() {
         install(new LoaderModule());
         install(new TranslationModule());
+        install(new SpawnModule());
     }
 
 }
