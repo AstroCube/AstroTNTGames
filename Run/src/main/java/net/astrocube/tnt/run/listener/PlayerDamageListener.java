452104b80fb27fca3e63fd28ad34a23280dfba70
Ian Castaño <ianfe@martina.com.co>
2021-03-04 10:23:49 -0500

    Now will check match before disqualification

--- Run/src/main/java/net/astrocube/tnt/run/listener/PlayerDamageListener.java
+++ Run/src/main/java/net/astrocube/tnt/run/listener/PlayerDamageListener.java
@@ -1,13 +1,24 @@
 package net.astrocube.tnt.run.listener;
 
+import com.google.inject.Inject;
+import net.astrocube.api.bukkit.game.match.ActualMatchCache;
+import net.astrocube.api.bukkit.game.match.control.MatchParticipantsProvider;
+import net.astrocube.api.bukkit.virtual.game.match.Match;
 import net.astrocube.tnt.event.PlayerDisqualificationEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Optional;
+import java.util.logging.Level;
 
 public class PlayerDamageListener implements Listener {
+
+    private @Inject ActualMatchCache actualMatchCache;
+    private @Inject Plugin plugin;
 
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent event) {
@@ -23,7 +34,21 @@ public class PlayerDamageListener implements Listener {
             return;
         }
 
-        Bukkit.getPluginManager().callEvent(new PlayerDisqualificationEvent(player));
+        try {
+
+            Optional<Match> match = actualMatchCache.get(player.getDatabaseIdentifier());
+
+            if (!match.isPresent() || !MatchParticipantsProvider.getOnlineIds(match.get()).contains(player.getDatabaseIdentifier())) {
+                event.setCancelled(true);
+                return;
+            }
+
+            Bukkit.getPluginManager().callEvent(new PlayerDisqualificationEvent(player));
+
+        } catch (Exception e) {
+            plugin.getLogger().log(Level.SEVERE, "There was an error while logging error.");
+            event.setCancelled(true);
+        }
 
     }
 
