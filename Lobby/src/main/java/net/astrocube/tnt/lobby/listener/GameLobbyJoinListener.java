9f531bfe362247d95b6a6dc94dd8370ad2baa98e
Ian Castaño <ianfe@martina.com.co>
2021-03-12 14:31:22 -0500

    Updated user listening

--- Lobby/src/main/java/net/astrocube/tnt/lobby/listener/GameLobbyJoinListener.java
+++ Lobby/src/main/java/net/astrocube/tnt/lobby/listener/GameLobbyJoinListener.java
@@ -1,12 +1,16 @@
 package net.astrocube.tnt.lobby.listener;
 
+import com.google.api.client.http.HttpResponseException;
 import com.google.inject.Inject;
+import net.astrocube.api.bukkit.game.exception.GameControlException;
 import net.astrocube.api.bukkit.lobby.event.LobbyJoinEvent;
+import net.astrocube.api.bukkit.virtual.game.match.MatchDoc;
 import net.astrocube.api.core.http.exception.NotFound;
 import net.astrocube.tnt.lobby.statistic.LobbyScoreboardProvider;
 import net.astrocube.tnt.shared.perk.PerkManifestProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
@@ -22,21 +26,28 @@ public class GameLobbyJoinListener implements Listener {
     @EventHandler
     public void onLobbyJoin(LobbyJoinEvent event) {
 
-        try {
-            perkManifestProvider.getManifest(event.getPlayer().getDatabaseIdentifier());
-        } catch (NotFound e) {
-            try {
-                perkManifestProvider.createRegistry(event.getPlayer().getDatabaseIdentifier(), plugin.getConfig().getString("registry.mode"));
-            } catch (Exception exception) {
-                plugin.getLogger().log(Level.SEVERE, "There was an error creating user perk manifest", e);
-                event.getPlayer().kickPlayer(ChatColor.RED + "Error while creating TNT Perk manifest.");
-            }
-        } catch (Exception e) {
-            plugin.getLogger().log(Level.SEVERE, "There was an error retrieving user perk manifest", e);
-            event.getPlayer().kickPlayer(ChatColor.RED + "Error while obtaining TNT Perk manifest.");
-        }
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            lobbyScoreboardProvider.setup(event.getPlayer());
 
-        Bukkit.getScheduler().runTask(plugin, () -> lobbyScoreboardProvider.setup(event.getPlayer()));
+            try {
+                perkManifestProvider.getManifest(event.getPlayer().getDatabaseIdentifier())
+                        .orElseThrow(() -> new GameControlException("Manifest not found"));
+            } catch (GameControlException e) {
+                try {
+                    perkManifestProvider.createRegistry(event.getPlayer().getDatabaseIdentifier(), plugin.getConfig().getString("registry.mode"));
+                } catch (Exception exception) {
+                    kickOnError(event.getPlayer(), e);
+                }
+            } catch (Exception e) {
+                kickOnError(event.getPlayer(), e);
+            }
+
+        });
+    }
+
+    private void kickOnError(Player player, Exception e) {
+        plugin.getLogger().log(Level.SEVERE, "There was an error retrieving user perk manifest", e);
+        player.kickPlayer(ChatColor.RED + "Error while obtaining TNT Perk manifest.");
     }
 
 }
--- Lobby/src/main/java/net/astrocube/tnt/lobby/loader/listener/UserListenerLoader.java
+++ Lobby/src/main/java/net/astrocube/tnt/lobby/loader/listener/UserListenerLoader.java
@@ -3,18 +3,21 @@ package net.astrocube.tnt.lobby.loader.listener;
 import com.google.inject.Inject;
 import net.astrocube.api.bukkit.server.ListenerLoader;
 import net.astrocube.tnt.lobby.listener.GameLobbyJoinListener;
+import net.astrocube.tnt.lobby.listener.GameLobbyLeaveListener;
 import org.bukkit.plugin.Plugin;
 
 public class UserListenerLoader implements ListenerLoader {
 
     private @Inject Plugin plugin;
     private @Inject GameLobbyJoinListener gameLobbyJoinListener;
+    private @Inject GameLobbyLeaveListener gameLobbyLeaveListener;
 
     @Override
     public void registerEvents() {
         registerEvent(
                 plugin,
-                gameLobbyJoinListener
+                gameLobbyJoinListener,
+                gameLobbyLeaveListener
         );
     }
 }
