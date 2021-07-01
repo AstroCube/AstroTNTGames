39064fecd0ba3df1010e4a622914a3572ea30300
yusshu <yushu@unnamed.team>
2021-07-01 14:34:47 -0500

    tntrun: make some other factors constants, also remove unnecessary list

--- run/src/main/java/net/astrocube/tnt/run/floor/CoreFloorRemover.java
+++ run/src/main/java/net/astrocube/tnt/run/floor/CoreFloorRemover.java
@@ -8,23 +8,54 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.bukkit.util.NumberConversions.floor;
 
 @Singleton
 public class CoreFloorRemover implements FloorRemover {
 
 	/**
-	 * Distance used when checking block below a player,
+	 * Distance used when checking blocks below a player,
 	 * if it's small, it won't remove floor check when
 	 * the distance is large, i.e. when a player jumps
 	 */
 	private static final float Y_DISTANCE_FACTOR = 0.8F;
 
+	/**
+	 * Distance used when checking blocks around and
+	 * below the player, it must be large enough to
+	 * make the player fall if they're over multiple
+	 * blocks
+	 */
+	private static final float HORIZONTAL_DISTANCE_FACTOR = 0.3F;
+
+	/**
+	 * Directions where the algorithm will check, the
+	 * arrays inside this matrix must have two as length,
+	 * representing X, and Y {@link CoreFloorRemover#HORIZONTAL_DISTANCE_FACTOR}
+	 * multipliers, it's all the possible combinations using
+	 * 1 and -1 by default.
+	 */
+	private static final int[][] CHECK_DIRECTIONS = new int[][]{
+			{1, 1}, {1, -1},
+			{-1, 1}, {-1, -1}
+	};
+
 	@Override
 	public void removeFloor(Location location) {
-		for (Block block : getBlocksBelow(location)) {
+
+		double x = location.getX();
+		double z = location.getZ();
+		World world = location.getWorld();
+		int y = floor(location.getY() - Y_DISTANCE_FACTOR);
+
+		for (int[] direction : CHECK_DIRECTIONS) {
+			Block block = world.getBlockAt(
+					floor(x + HORIZONTAL_DISTANCE_FACTOR * direction[0]),
+					y,
+					floor(z + HORIZONTAL_DISTANCE_FACTOR * direction[1])
+			);
 			Block blockBelow = block.getRelative(BlockFace.DOWN);
+
 			if ((blockBelow.getType() == Material.TNT)
 					&& (block.getType() == Material.SAND || block.getType() == Material.GRAVEL)) {
 				removeBlockMagically(block);
@@ -36,46 +67,6 @@ public class CoreFloorRemover implements FloorRemover {
 	private void removeBlockMagically(Block block) {
 		block.setType(Material.AIR);
 		block.getWorld().playEffect(block.getLocation().add(.5, .5, .5), Effect.SMOKE, 1, 1);
-	}
-
-	private List<Block> getBlocksBelow(Location entityLocation) {
-
-		List<Block> blocksBelow = new ArrayList<>();
-		double x = entityLocation.getX();
-		double z = entityLocation.getZ();
-		World world = entityLocation.getWorld();
-		double yBelow = entityLocation.getY() - Y_DISTANCE_FACTOR;
-
-		Block northEast = new Location(world, x + 0.3, yBelow, z - 0.3).getBlock();
-		Block northWest = new Location(world, x - 0.3, yBelow, z - 0.3).getBlock();
-		Block southEast = new Location(world, x + 0.3, yBelow, z + 0.3).getBlock();
-		Block southWest = new Location(world, x - 0.3, yBelow, z + 0.3).getBlock();
-		Block[] blocks = {northEast, northWest, southEast, southWest};
-
-		for (Block block : blocks) {
-
-			if (!blocksBelow.isEmpty()) {
-
-				boolean duplicateExists = false;
-
-				for (Block value : blocksBelow) {
-					if (value.equals(block)) {
-						duplicateExists = true;
-						break;
-					}
-				}
-
-				if (!duplicateExists) {
-					blocksBelow.add(block);
-				}
-
-			} else {
-				blocksBelow.add(block);
-			}
-		}
-
-		return blocksBelow;
-
 	}
 
 }
