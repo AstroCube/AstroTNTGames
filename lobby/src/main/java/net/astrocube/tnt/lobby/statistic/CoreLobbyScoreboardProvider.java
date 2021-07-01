296a7d0cf6a3f2fbece3de8ea54157410d83be0b
yusshu <yushu@unnamed.team>
2021-07-01 13:59:16 -0500

    lobby(board): use latest commons scoreboard system

--- lobby/src/main/java/net/astrocube/tnt/lobby/statistic/CoreLobbyScoreboardProvider.java
+++ lobby/src/main/java/net/astrocube/tnt/lobby/statistic/CoreLobbyScoreboardProvider.java
@@ -4,29 +4,29 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.yushust.message.MessageHandler;
 import me.yushust.message.util.StringList;
-import net.astrocube.api.bukkit.board.ScoreboardManagerProvider;
+import net.astrocube.api.bukkit.board.Board;
+import net.astrocube.api.bukkit.board.BoardProvider;
 import net.astrocube.tnt.shared.money.MoneyTransactionHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import team.unnamed.uboard.ScoreboardObjective;
-import team.unnamed.uboard.builder.ScoreboardBuilder;
-
-import java.util.Optional;
 
 @Singleton
 public class CoreLobbyScoreboardProvider implements LobbyScoreboardProvider {
 
 	private @Inject MessageHandler messageHandler;
-	private @Inject ScoreboardManagerProvider scoreboardManagerProvider;
 	private @Inject MoneyTransactionHandler moneyTransactionHandler;
 	private @Inject ModeVictoryProvider modeVictoryProvider;
+	private @Inject BoardProvider boardProvider;
 	private @Inject Plugin plugin;
 
 	@Override
 	public void setup(Player player) {
 
-		Optional<ScoreboardObjective> objectiveOptional =
-				scoreboardManagerProvider.getScoreboard().getScoreboard("tntlobby_" + player.getDatabaseIdentifier());
+		Board board = boardProvider.get(player)
+				.orElseGet(() -> boardProvider.create(
+						player,
+						messageHandler.get(player, "board.title")
+				));
 
 
 		StringList scoreTranslation = messageHandler.replacingMany(
@@ -48,17 +48,7 @@ public class CoreLobbyScoreboardProvider implements LobbyScoreboardProvider {
 
 		);
 
-		if (!objectiveOptional.isPresent()) {
-			ScoreboardBuilder builder =
-					scoreboardManagerProvider.getScoreboard().newScoreboard("tntlobby_" + player.getDatabaseIdentifier());
-			scoreTranslation.forEach(builder::addLine);
-			builder.setTitle(messageHandler.get(player, "board.title"));
-			scoreboardManagerProvider.getScoreboard().setToPlayer(player, builder.build());
-		} else {
-			objectiveOptional.get().setStringLines(scoreTranslation);
-			objectiveOptional.get().updateScoreboard();
-		}
-
+		board.setLines(scoreTranslation);
 	}
 
 }
